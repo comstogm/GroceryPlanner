fda73f8c03f3bf866da3f1fd6e4ceb8bbd0c5c99
Gabriel Comstock <comstogm@mail.uc.edu>
2023-09-10 11:12:14 -0400

    Add DTO Class, basic controller mapping

--- src/main/java/com/enterprise4045/groceryplanner/GroceryPlannerController.java
+++ src/main/java/com/enterprise4045/groceryplanner/GroceryPlannerController.java
@@ -1,7 +1,10 @@
 package com.enterprise4045.groceryplanner;
 
+import dto.LoggedItem;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GroceryPlannerController {
@@ -11,5 +14,37 @@ public class GroceryPlannerController {
     @RequestMapping("/")
     public String index() {
         return "start";
+    }
+
+    /*
+    Fetches all logged items
+     */
+    @GetMapping("/loggeditem/")
+    public ResponseEntity fetchallLoggedItems() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    /*
+    Fetches logged item by id
+     */
+    @GetMapping("/loggeditem/{id}/")
+    public ResponseEntity fetchLoggedItemById(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    /*
+    Creates a new item
+     */
+    @PostMapping(value="/loggeditem", consumes="appplication/json", produces ="application/json")
+    public LoggedItem createLoggedItem(@RequestBody LoggedItem loggedItem) {
+        return loggedItem;
+    }
+
+    /*
+    Deletes and item based on id
+     */
+    @DeleteMapping("/loggeditem/{id}/")
+    public ResponseEntity deleteLoggedItem(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
---
+++ src/main/java/dto/LoggedItem.java
@@ -0,0 +1,9 @@
+package dto;
+
+import lombok.Data;
+
+public @Data class LoggedItem {
+    private int itemId;
+    private String LoggedItemId;
+    private String Description;
+}
