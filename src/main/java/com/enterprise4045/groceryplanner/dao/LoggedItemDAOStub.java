265e758b67e8a69500efd8233871edc29ea7756e
Gabriel Comstock <comstogm@mail.uc.edu>
2023-10-15 12:48:26 -0400

    Constructor injection, cleaned up some return data

--- src/main/java/com/enterprise4045/groceryplanner/dao/LoggedItemDAOStub.java
+++ src/main/java/com/enterprise4045/groceryplanner/dao/LoggedItemDAOStub.java
@@ -9,7 +9,11 @@ import java.util.List;
 
 @Repository
 public class LoggedItemDAOStub implements ILoggedItemDAO{
+
+    //Creates a new hash map when instantiating
     HashMap<Integer, LoggedItem> allLoggedItem = new HashMap<>();
+
+    //Gets the key of the hashMap and adds the items to the database
     @Override
     public LoggedItem save(LoggedItem loggedItem) {
         Integer loggedItemID = Integer.parseInt(loggedItem.getLoggedItemId());
@@ -17,17 +21,20 @@ public class LoggedItemDAOStub implements ILoggedItemDAO{
         return loggedItem;
     }
 
+    //Gets all the loggedItems and returns them into a list
     @Override
     public List<LoggedItem> fetchAll() {
         List<LoggedItem> returnLoggedItem = new ArrayList<>(allLoggedItem.values());
         return returnLoggedItem;
     }
 
+    //Returns the loggedItem, given the ID
     @Override
     public LoggedItem fetch(int id) {
         return allLoggedItem.get(id);
     }
 
+    //Deletes the loggedItem from the database, given the ID
     @Override
     public void delete(int id) {
         allLoggedItem.remove(id);
