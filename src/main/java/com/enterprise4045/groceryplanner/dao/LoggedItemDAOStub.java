0a62ab54617a824b2754c6748122f9e21286a069
Kelli Porter <kelli.porter1998@gmail.com>
2023-10-17 12:48:33 -0400

    Updated Block Comments
    Added proper Block Comments to "LoggedItemDAOStub.java"

--- src/main/java/com/enterprise4045/groceryplanner/dao/LoggedItemDAOStub.java
+++ src/main/java/com/enterprise4045/groceryplanner/dao/LoggedItemDAOStub.java
@@ -9,7 +9,17 @@ import java.util.List;
 
 @Repository
 public class LoggedItemDAOStub implements ILoggedItemDAO{
+    /*
+    * Create a new HashMap
+    * Stores an Integer and a LoggedItem
+    */
     HashMap<Integer, LoggedItem> allLoggedItem = new HashMap<>();
+
+    /*
+    * Saves LoggedItem to the HashMap
+    * Pars the Integer and set the Integer to be a loggedItemID
+    * Add loggedItemID and loggedItem to the HashMap
+    */
     @Override
     public LoggedItem save(LoggedItem loggedItem) {
         Integer loggedItemID = Integer.parseInt(loggedItem.getLoggedItemId());
@@ -17,17 +27,25 @@ public class LoggedItemDAOStub implements ILoggedItemDAO{
         return loggedItem;
     }
 
+    /*
+    * Returns a list of all items in the HashMap
+    */
     @Override
     public List<LoggedItem> fetchAll() {
-        List<LoggedItem> returnLoggedItem = new ArrayList<>(allLoggedItem.values());
-        return returnLoggedItem;
+        return new ArrayList<>(allLoggedItem.values());
     }
 
+    /*
+     * Returns a specific item from the HashMap
+     */
     @Override
     public LoggedItem fetch(int id) {
         return allLoggedItem.get(id);
     }
 
+    /*
+     * Deletes a specific item from the HashMap
+     */
     @Override
     public void delete(int id) {
         allLoggedItem.remove(id);
