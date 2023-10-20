208fab7396840dee3c05a411de5f915a47fb7ac5
Jackson Pinchot <jackson_pinchot@yahoo.com>
2023-10-20 00:26:45 -0400

    added class so program can compile.

--- src/main/java/com/enterprise4045/groceryplanner/dto/LoggedItem.java
+++ src/main/java/com/enterprise4045/groceryplanner/dto/LoggedItem.java
@@ -6,4 +6,8 @@ public @Data class LoggedItem {
     private int itemId;
     private String loggedItemId;
     private String description;
+
+    public String getLoggedItemId() {
+        return null;
+    }
 }
