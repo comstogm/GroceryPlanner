b74c125a722dc2350b76e601da70ab7acbd02aad
Gabriel Comstock <comstogm@mail.uc.edu>
2023-11-19 13:54:16 -0500

    <Pattern>%d %p %C{1.} [%t] %m%n</Pattern>

--- src/test/java/com/enterprise4045/groceryplanner/GroceryPlannerApplicationTests.java
+++ src/test/java/com/enterprise4045/groceryplanner/GroceryPlannerApplicationTests.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ExecutionException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@SpringBootTest(classes=GroceryPlannerApplication.class)
 class GroceryPlannerApplicationTests {
 
 
